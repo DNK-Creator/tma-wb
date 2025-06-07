ee50a06e707354da9fd857584ecbae49c82eecf1
DNK-Creator <korotaevnikita73@gmail.com>
2025-06-08 00:10:14 +0300

    tutorial-done

---
+++ bot/app.js
@@ -0,0 +1,15 @@
+import {Telegraf, Markup, Context} from "telegraf"
+const token = "8002815581:AAHwT9Hx8aJneo9de7T6S2YB_INEy6XHY1A"
+const webAppUrl = "https://testwebapp300.web.app"
+
+const bot = new Telegraf(import.meta.env.VITE_BOT_TOKEN)
+
+bot.command("start", (ctx)=> {
+ctx.reply("Test your luck! You know luck is a strange thing! When you need it the most it just might not be there.", 
+    Markup.inlineKeyboard([
+        Markup.button.webApp( "Spin the wheel", `${webAppUrl}?ref=${ctx.payload}`
+        ),
+    ]));
+})  
+
+bot.launch()
